ddf5a6aae69a62a75de554950f55af28a02fcd01
Pedro Freitas de Lima <pedro.freitas.lima.97@gmail.com>
2024-09-10 18:34:53 -0300

    CHORE: create display highest, lowest, average prices

--- Services/Concretes/ConsumerStreamingEthUsd.cs
+++ Services/Concretes/ConsumerStreamingEthUsd.cs
@@ -41,20 +41,86 @@ public class ConsumerStreamingEthUsd : IConsumeStreaming
             {
                 string dataStream = Encoding.ASCII.GetString(buffer, 0, result.Count);
                 LiveOrderBook liveOrderBook = JsonSerializer.Deserialize<LiveOrderBook>(dataStream) ?? throw new InvalidOperationException();
-                await DisplayDataAnalytics();
-                await PersistData();
+                DisplayDataAnalytics(liveOrderBook.bids, liveOrderBook.asks);
+                await PersistData(liveOrderBook);
             }
         }
     }
 
-    private async Task DisplayDataAnalytics()
+    private void DisplayDataAnalytics(Bid[] bids, Ask[] asks)
+    {
+        ShowHighestPrice(bids, asks);
+        ShowLowestPrice(bids, asks);
+        ShowLastAveragePrice(bids, asks);
+        ShowAveragePriceNow(bids, asks);
+    }
+    
+    private async Task PersistData(LiveOrderBook liveOrderBook)
     {
         await Task.CompletedTask;
     }
-    
-    private async Task PersistData()
+
+    private void ShowHighestPrice(Bid[] bids, Ask[] asks)
     {
-        await Task.CompletedTask;
+        var highestPriceBid = bids
+            .ToList()
+            .AsReadOnly()
+            .MaxBy(b => b.usdValue);
+        
+        var highestPriceAks = asks
+            .ToList()
+            .AsReadOnly()
+            .MaxBy(b => b.usdValue);
+        
+        Console.WriteLine($"The highest price of the bid now is: {highestPriceBid?.usdValue}");
+        Console.WriteLine($"The highest price of the ask now is: {highestPriceAks?.usdValue}");
+    }
+    
+    private void ShowLowestPrice(Bid[] bids, Ask[] asks)
+    {
+        var lowestPriceBid = bids
+            .ToList()
+            .AsReadOnly()
+            .MinBy(b => b.usdValue);
+        
+        var lowestPriceAsk = asks
+            .ToList()
+            .AsReadOnly()
+            .MaxBy(b => b.usdValue);
+        
+        Console.WriteLine($"The highest price of the bid now is: {lowestPriceBid?.usdValue}");
+        Console.WriteLine($"The highest price of the ask now is: {lowestPriceAsk?.usdValue}");
+    }
+    private void ShowLastAveragePrice(Bid[] bids, Ask[] asks)
+    {
+        var averagePriceBid = bids
+            .ToList()
+            .AsReadOnly()
+            .Average(b =>  Convert.ToDecimal(b.usdValue));
+        
+        var averagePriceAsk = asks
+            .ToList()
+            .AsReadOnly()
+            .Average(b => Convert.ToDecimal(b.usdValue));
+        
+        Console.WriteLine($"The last average price of the bid is: {averagePriceBid:C}");
+        Console.WriteLine($"The last average price of the ask is: {averagePriceAsk:C}");
+    }
+    
+    private void ShowAveragePriceNow(Bid[] bids, Ask[] asks)
+    {
+        var averagePriceBid = bids
+            .ToList()
+            .AsReadOnly()
+            .Average(b =>  Convert.ToDecimal(b.usdValue));
+        
+        var averagePriceAsk = asks
+            .ToList()
+            .AsReadOnly()
+            .Average(b => Convert.ToDecimal(b.usdValue));
+        
+        Console.WriteLine($"The average price of the bid now is: {averagePriceBid:C}");
+        Console.WriteLine($"The average price of the ask now is: {averagePriceAsk:C}");
     }
     
     
